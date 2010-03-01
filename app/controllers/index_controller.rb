1e7decb5b44368fd2d30ba76e02a516902ddd0a5
Olexiy Zamkoviy <olexiy.z@gmail.com>
2010-03-01 23:22:17 +0200

    Large amount of fixes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,6 @@
 class IndexController < ApplicationController
   def index
-    @menu_items = Category.all(:order => :position)
+    load_menu
 
     if params[:category]
       @category = Category.find params[:category]
@@ -11,18 +11,43 @@ class IndexController < ApplicationController
       @category = Category.first :order => :position
     end
 
-    if @category.show_answers
-      @answers = Answer.random_two
-    else
-      @answers = []
-    end
-
-    if @category.show_gallery
-      @images  = Image.random_three
-    else
-      @images = []
-    end
-
-    @pages =@category.pages
+    load_additional_data
   end
+  
+  def search
+    load_menu
+    @pages = []
+
+    if params[:q]
+      @results = []
+    end
+  end
+
+  protected
+  
+    def load_menu
+      @menu_items = Category.all(:order => :position)
+    end
+
+    def load_category
+    end
+
+    def load_additional_data
+      if @category.show_answers
+        @answers = Answer.random_two
+      else
+        @answers = []
+      end
+
+      if @category.show_gallery
+        @images  = Image.random_three
+      else
+        @images = []
+      end
+
+      @pages =@category.pages
+      if @pages.size == 1
+        @page = @pages.first
+      end
+    end
 end
